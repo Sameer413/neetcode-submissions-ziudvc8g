cf2ade27d6370259dbaff73288542c5a6e285638
sameer nimje <92072016+Sameer413@users.noreply.github.com>
2026-04-09 13:27:31 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int d) {
+        int minCap = 0;
+        int maxCap = 0;
+        
+        for(int weight : weights){
+            minCap = Math.max(minCap, weight);
+            maxCap += weight;
+        }
+
+        while(minCap < maxCap){
+            int mid = minCap + (maxCap - minCap) / 2;
+
+            int days = 1;
+            int sum = 0;
+
+            for(int weight : weights){
+                if(sum + weight > mid){
+                    days++;
+                    sum = 0;
+                }
+                sum += weight;
+            }
+
+            if(days > d){
+                minCap = mid + 1;
+            }else{
+                maxCap = mid;
+            }
+        }
+
+        return minCap;
+    }
+}
